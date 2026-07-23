1198b4f85f12f20e2b26c33849cc3e4ec0f9a437
Miguelin04 <miguel.a.luna@unl.edu.ec>
2026-07-23 12:20:23 -0500

    cambios

--- infraestructura/balanceador/application/health_monitor.py
+++ infraestructura/balanceador/application/health_monitor.py
@@ -39,7 +39,7 @@ class HealthMonitor:
             # Aislando la persistencia de la lógica de red
             for srv, changed in results.items():
                 if changed:
-                    self.db.update_node_state(srv.id, srv.status, srv.url)
+                    self.db.update_node_state(srv.id, srv.status, srv.address)
 
             time.sleep(self.interval)
 
